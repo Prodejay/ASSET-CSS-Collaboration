ca340264d49eb16abadf2c974d5ec339582ae101
JJPadilla <jjpadilla0915@gmail.com>
2021-04-04 20:29:45 +0800

    progress on map activity

---
+++ ASSET CSS Collaboration Project/Assets/Scripts/Activities/MapActivity/Country.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Country : MonoBehaviour
+{
+    public string[] correctAnswer;
+
+    public TMP_InputField inputAnswer;
+
+    public bool answeredCorrectly;
+
+    public void checkAnswer()
+    {
+        string answer = inputAnswer.text;
+
+        foreach(string possibleAnswer in correctAnswer)
+        {
+            if(answer == possibleAnswer)
+            {
+
+            }
+        }
+    }
+}
---
+++ ASSET CSS Collaboration Project/Assets/Scripts/Activities/MapActivity/MapActivityController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MapActivityController : MonoBehaviour
+{
+    public static MapActivityController instance;
+
+    public int score = 0;
+
+    public TextMeshProUGUI scoreText;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void addScore()
+    {
+        score++;
+
+        scoreText.text = score + "/11";
+    }
+}
